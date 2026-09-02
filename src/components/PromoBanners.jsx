79eac6732a6488b75deee1b9d181036ceb78791b
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-09-02 09:43:35 +0530

    Completed PromoBanner Section UI

--- src/components/PromoBanners.jsx
+++ src/components/PromoBanners.jsx
@@ -1,4 +1,5 @@
 import Cookie from "../assets/Cookies6.webp";
+import CakeSlice from "../assets/CakeSlice3.webp";
 const PromoBanners = () => {
   return (
     <section className="mx-auto max-w-[1500px] px-6 py-10">
@@ -28,6 +29,26 @@ const PromoBanners = () => {
         </div>
 
         {/* Right Side */}
+        <div className="relative overflow-hidden rounded-3xl bg-pink-200 px-8 py-10">
+          <div className="relative z-10 max-w-[55%] mt-14 lg:mt-2">
+            <p className="text-lg font-semibold text-gray-700">
+              Deal of the Day
+            </p>
+            <p className="mt-2 text-4xl font-semibold text-rose-400">20%OFF</p>
+            <p className="mt-1 font-semibold text-gray-800 text-lg">
+              On All Pastries
+            </p>
+            <button className="mt-6 rounded-full bg-rose-400 px-6 py-3 text-sm font-semibold text-white transition-colors hover:bg-rose-600">
+              Grab the Deal
+            </button>
+          </div>
+
+          <img
+            src={CakeSlice}
+            alt="Cake Slice"
+            className="absolute -right-2 lg:bottom-8 bottom-18 w-50 object-contain md:w-60"
+          />
+        </div>
       </div>
     </section>
   );
